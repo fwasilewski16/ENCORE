9fbf686de49559755e7ca9749d2ead43a0b1a3b3
Filip Wasilewski <filipwasilewski1@gmail.com>
2023-12-29 15:03:38 +0000

    add all

---
+++ src/hooks/useFetchEvents.jsx
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { eventsActions } from "../store";
+import { useEffect } from "react";
+
+export default function useFetchEvents() {
+  const dispatch = useDispatch();
+
+  const { city, dateFrom, dateTo } = useSelector((state) => state.events.filter);
+
+  useEffect(() => {
+    async function fetchEvents(city, dateFrom, dateTo) {
+      try {
+        dispatch(eventsActions.isLoadingHandler(true));
+        const response = await fetch(`https://portfolio-backend-409515.lm.r.appspot.com/encore/events?city=${city}&dateFrom=${dateFrom}&dateTo=${dateTo}&artist_id=`);
+        if (!response.ok) {
+          throw Error("Something went wrong");
+        }
+        const data = await response.json();
+        dispatch(eventsActions.isLoadingHandler(false));
+        dispatch(eventsActions.addEvents(data));
+      } catch (error) {
+        dispatch(eventsActions.isLoadingHandler(false));
+        dispatch(eventsActions.errorHandler(error.message));
+      }
+    }
+    fetchEvents(city, dateFrom, dateTo);
+  }, [city, dateFrom, dateTo, dispatch]);
+
+  return [city, dateFrom, dateTo];
+}
---
+++ src/hooks/useFetchSingleArtist.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+export default function useFetchSingleArtist(artist_id) {
+  const [artist, setArtist] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    async function fetchArtist(artist_id) {
+      try {
+        setIsLoading(true);
+        const responseArtist = await fetch(`https://portfolio-backend-409515.lm.r.appspot.com/encore/artists/${artist_id}`);
+        const dataArtist = await responseArtist.json();
+        setArtist(dataArtist);
+        const responseEvents = await fetch(`https://portfolio-backend-409515.lm.r.appspot.com/encore/events?artist_id=${artist_id}`);
+        const dataEvents = await responseEvents.json();
+        setEvents(dataEvents);
+        setIsLoading(false);
+      } catch (error) {
+        setError(true);
+      }
+    }
+    fetchArtist(artist_id);
+  }, [artist_id]);
+
+  return [artist, isLoading, error, events];
+}
